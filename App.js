23c3535e373709fd53b9211dd830b44647a4bb59
Prabeen04 <prabeen.strange@gmail.com>
2018-03-23 09:22:38 +0530

    react native configuration implemented

--- App.js
+++ App.js
@@ -1,20 +1,48 @@
 import React from 'react';
 import { View, Text } from 'react-native'
-import {Container, Button, Left, Right } from 'native-base';
+import {Container, Header, Button, Left, Right, Content, Footer, FooterTab, Icon } from 'native-base';
+import  MyHeader  from './components/common-components/header/header'
+import Expo from 'expo';
 export default class App extends React.Component {
   constructor(props) {
     super(props)
+    this.state = { loading: true };
   }
   async componentWillMount() {
     await Expo.Font.loadAsync({
-      'Roboto': require('native-base/Fonts/Roboto.ttf'),
-      'Roboto_medium': require('native-base/Fonts/Roboto_medium.ttf'),
+      Roboto: require("native-base/Fonts/Roboto.ttf"),
+      Roboto_medium: require("native-base/Fonts/Roboto_medium.ttf"),
+      Ionicons: require("@expo/vector-icons/fonts/Ionicons.ttf"),
     });
+    this.setState({ loading: false });
   }
+
   render() {
+    if (this.state.loading) {
+      return <Expo.AppLoading />;
+    }
     return (
       <Container>
-        <Text>Google Plus</Text>
+       <MyHeader />
+        <Content>
+         <Text>Google Plus</Text>
+        </Content>
+        <Footer>
+          <FooterTab>
+            <Button>
+              <Icon name="apps" />
+            </Button>
+            <Button>
+              <Icon name="camera" />
+            </Button>
+            <Button active>
+              <Icon active name="navigate" />
+            </Button>
+            <Button>
+              <Icon name="person" />
+            </Button>
+          </FooterTab>
+          </Footer>
       </Container>
     );
   }
