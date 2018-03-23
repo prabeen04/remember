ac41b01090fe4360590c56a10eeb8bffc452739e
Prabeen04 <prabeen.strange@gmail.com>
2018-03-23 09:15:22 +0530

    application redesigned from scratch

--- App.js
+++ App.js
@@ -1,34 +1,21 @@
 import React from 'react';
-import MyHeader from './components/common-components/header/header';
-import MyTab from './components/tabs/my-tabs';
-import MyFooter from './components/common-components/footer/footer';
-import { Container } from 'native-base';
-import Expo from "expo";
-
+import { View, Text } from 'react-native'
+import {Container, Button, Left, Right } from 'native-base';
 export default class App extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props)
-    this.state = { loading: true };
   }
   async componentWillMount() {
     await Expo.Font.loadAsync({
-      Roboto: require("native-base/Fonts/Roboto.ttf"),
-      Roboto_medium: require("native-base/Fonts/Roboto_medium.ttf"),
-      Ionicons: require("@expo/vector-icons/fonts/Ionicons.ttf"),
+      'Roboto': require('native-base/Fonts/Roboto.ttf'),
+      'Roboto_medium': require('native-base/Fonts/Roboto_medium.ttf'),
     });
-    this.setState({ loading: false });
   }
-
   render() {
-    if (this.state.loading) {
-      return <Expo.AppLoading />;
-    }
     return (
       <Container>
-      <MyHeader />
-      <MyTab/>
-      <MyFooter/>
-    </Container>
+        <Text>Google Plus</Text>
+      </Container>
     );
   }
 }
